05388e3c9bc47626830f4d3643653d7508a98fbd
Yibo Duan <yibo.duan@gmail.com>
2015-11-21 16:20:21 -0500

    incomplete angel api call

--- app/routes/api/placecalls.js
+++ app/routes/api/placecalls.js
@@ -2,10 +2,16 @@
 
 var express = require('express');
 var xml     = require('xml');
+var request = require('request-promise');
+var Promise = require('bluebird');
 var router = express.Router();
 
 var PhoneNumber = require('../../models/PhoneNumber');
 
+var subscriber_id = "0a140225-04-150f39bbbf8-06dfc85a-1b2";
+var site_code = "200000149531";
+var api_key = "0a1414cd-0b-150f91db224-2b7fdec6-71e@0a140225-04-150f39bbbf8-06dfc85a-1b2@c631b4fe@1447288484388@10@b6233f2947910a588d681bfe697243bb"
+var base_url = "https://api.angel.com/outbound-rest/"
 router.post('/', function(req, res) {
 
     var category = req.body.category;
@@ -26,7 +32,22 @@ router.post('/', function(req, res) {
             console.log(item.number);
         });
         console.log(out);
-        return res.send(xml(out));
+        var api_call = request({
+            method: 'POST',
+            uri: base_url + subscriber_id + "/" + site_code + "/job?apiKey=" + api_key,
+            headers: {
+                'Content-type': 'application/xml'
+            },
+            data: xml(out)
+        });
+
+        api_call.then(function(data){
+            console.log(data);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+        // return res.send(xml(out));
       });
 
 
